88b4fac21bfd9e4042f87736c0cf18d4e0264ff4
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2023-01-14 11:41:13 +0100

    forms separated

---
+++ CRM/Helpers/ComponentMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace CRMBlazor.Helpers
+{
+    internal sealed class ComponentMetadata
+    {
+        public Type ComponentType { get; set; }
+        public Dictionary<string, object> ComponentParameters { get; set; }
+    }
+}
